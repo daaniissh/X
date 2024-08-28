004512deba691c352586496eb4b99fee70dc80f4
daaniissh <pkdanish329@gmail.com>
2024-08-28 17:51:31 +0530

    fix:Cookies error

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ import cookieParser from "cookie-parser";
 dotenv.config();  // Load environment variables
 
 const app = express();  // Initialize the express application
-
+app.use(cookieParser());
 // Middleware Setup
 app.use(express.json({ limit: "5mb" }));  // Limit the size of JSON payloads
 app.use(cors({
@@ -20,7 +20,7 @@ app.use(cors({
   credentials: true, // Allow credentials (cookies) to be sent
 }));
 
-app.use(cookieParser());
+
 app.use(express.urlencoded({ extended: true }));
 
 // Cloudinary Configuration
