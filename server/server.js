19286b4ab494a752ab443c5b7628695d8ab449e4
daaniissh <pkdanish329@gmail.com>
2024-08-28 21:12:30 +0530

    update:cors orgin

--- server/server.js
+++ server/server.js
@@ -15,19 +15,10 @@ const app = express();  // Initialize the express application
 
 // Middleware Setup
 app.use(express.json({ limit: "5mb" }));  // Limit the size of JSON payloads
-const allowedOrigins = ['https://x-client-home.vercel.app', 'http://localhost:3000/'];
-
 app.use(cors({
-  origin: function (origin, callback) {
-    if (allowedOrigins.includes(origin) || !origin) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
+  origin: 'http://localhost:3000/', // Replace with your frontend domain
   credentials: true, // Allow credentials (cookies) to be sent
 }));
-
 
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
