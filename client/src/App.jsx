c6d6fcb66a7b4bfe95bd466af8e5ebaff02fcacd
daaniissh <pkdanish329@gmail.com>
2024-08-28 17:46:49 +0530

    fix:Cookies error

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -19,11 +19,8 @@ function App() {
     queryFn: async () => {
       try {
         const res = await fetch("https://x-nu-murex.vercel.app/api/auth/me", {
-          method: "GET",
           credentials: "include", 
-          headers: {
-            'Accept': 'application/json',
-          }
+         
         });
         const data = await res.json()
         if (data.error) return null
